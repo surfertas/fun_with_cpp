65d3f8ddf4a623b8e8505fc7d7e23b09ada5dcad
surfertas <tasuku@gmail.com>
2018-07-24 00:19:42 +0900

    trying to use cpp14

--- project_euler/3-largest_prime_factor/main.cpp
+++ project_euler/3-largest_prime_factor/main.cpp
@@ -19,19 +19,18 @@ void usage()
 int main(int argc, char** argv)
 {
   // Arbitrarily set. Need to find a programmatic way to handle.
-  const auto scale_max = 50000;//00000;
+  const auto scale_max { 50000 };//00000;
 
   if (argc < 2) { usage(); }
-
   auto target = std::atoll(argv[1]);
-  std::vector<size_t> scale(scale_max);
+  auto scale = std::vector<size_t>(scale_max);
   std::iota(scale.begin(), scale.end(), 2);
   std::unordered_set<size_t> primes;
   std::vector<size_t> factors;
 
   prime_util::PrimeUtil p;
   for (auto x : scale) {
-    if (p.is_prime(x) == 0) {
+    if (p.is_prime(x)) {
       primes.insert(x);
       while ((target % x) == 0) {
         factors.emplace_back(x);
