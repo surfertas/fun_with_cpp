59b354b00ce0d4332a2ce5f2513466e7bf1129c2
surfertas <tasuku@gmail.com>
2019-05-01 22:01:04 +0900

    use cmake

---
+++ project_euler/9-special-pythagorean-triplet/src/main.cpp
@@ -0,0 +1,44 @@
+// @author Tasuku Miura
+
+#include <iostream>
+#include <tuple>
+
+bool isPythogoreanTriplet(const size_t a, const size_t b, const size_t c) {
+  auto testOne = a < b && b < c;
+  auto testTwo = (a*a + b*b) == c*c;
+  return testOne && testTwo;
+}
+
+std::tuple<size_t, size_t, size_t> generatePythogoreanTriplet(const size_t m, const size_t n) {
+  auto a = 2 * m * n;
+  auto b = m * m - n * n;
+  auto c = m * m + n * n;
+  std::cout << "a:" << a << " " << b << " "  << c << "\n";
+  std::tuple<size_t, size_t, size_t> triplet(a,b,c);
+
+  return triplet;
+}
+
+int main(int argc, char** argv)
+{
+  auto m = 2;
+  while (true) {
+    for (auto n = 1; n <= m; n++) {
+      auto triplet = generatePythogoreanTriplet(m,n);
+      auto a = std::get<0>(triplet);
+      auto b = std::get<1>(triplet);
+      auto c = std::get<2>(triplet);
+
+      auto isTriplet = isPythogoreanTriplet(a, b, c);
+      if (isTriplet) {
+        std::cout << a << " " << b << " "  << c << "\n";
+        if ((a + b + c) == 1000) {
+          std::cout  << " Solution: " << a * b * c << "\n";
+          break;
+        }
+      }
+    }
+    break;
+    m++;
+  }
+}
