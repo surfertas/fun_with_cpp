58f867d573285d9f4a602adc55e0cf04eafbe5a4
surfertas <tasuku@gmail.com>
2019-05-01 21:50:56 +0900

    use cmake

--- project_euler/7-10001st-prime/src/main.cpp
+++ project_euler/7-10001st-prime/src/main.cpp
@@ -4,15 +4,15 @@
 // What is the 10 001st prime number?
 
 #include <iostream>
-#include "libprime.h"
+#include <primeutil.h>
 
 int main(int argc, char** argv)
 {
-  size_t prime_count = 0;
-  size_t i = 2;
+  auto prime_count = 0;
+  auto i = 2;
   prime_util::PrimeUtil p;
   while (true) {
-    if (p.is_prime(i) == 0) {
+    if (p.is_prime(i) == true) {
       prime_count++;
       if (prime_count == 10001) {
         break;
